31e8a9c58fa7f6c146e19bb9ca231b53cf77a579
David Valachovic <David.E.Valachovic@gmail.com>
2026-05-17 12:35:02 -0500

    feat: update icon references in manifest and remove unused images

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,12 +3,10 @@ import streamDeck from '@elgato/streamdeck';
 import { MixerNextPageAction, MixerPreviousPageAction } from './actions/mixer-page';
 import { MixerSlotAction } from './actions/mixer-slot';
 
-// We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel("trace");
 
 streamDeck.actions.registerAction(new MixerSlotAction());
 streamDeck.actions.registerAction(new MixerPreviousPageAction());
 streamDeck.actions.registerAction(new MixerNextPageAction());
 
-// Finally, connect to the Stream Deck.
 streamDeck.connect();
